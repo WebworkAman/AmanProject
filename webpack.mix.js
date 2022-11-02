27af9af2f8e80eab9523a48fd023b2f42186487c
webwork.aman <webwork.aman@gmail.com>
2022-11-02 17:32:33 +0800

    React insert Laravel

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,21 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/css/app.scss', 'public/css')
-    .sass('resources/css/content.scss', 'public/css')
-    .sass('resources/css/register.scss', 'public/css')
-    .sass('resources/css/log.scss', 'public/css')
-    .copyDirectory('resources/imgs','public/imgs');
-
-// mix.styles([
-//     'resources/css/a.css',
-//     'resources/css/b.css',
-// ],'public/css/abc.min.css')
-// .copyDirectory('resources/imgs','public/imgs');     
-
+    .react()
+    .sass('resources/sass/app.scss', 'public/css');
