cfa3b56bef6271253df005df85e1cce69ebdb70d
webwork.aman <webwork.aman@gmail.com>
2022-11-25 13:28:15 +0800

    ? icon加入版面

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,83 +5,138 @@
         <div class="img-block">
             <ul>
                 <li><a href="/inspection"><img src="../imgs/1596784113.png"></img></a>
-                    <p>驗 布</p>
+                    <ol>
+                        <li><p>驗 布</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
+                    
                 </li>
                 <li><a href="/relaxing"><img src="../imgs/1597907651.png"></a>
-                    <p>鬆 布</p>
+                    
+                    <ol>
+                        <li><p>鬆 布</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li><a href="/spreading"><img src="../imgs/1596784246.png"></a>
-                    <p>拉 布</p>
+                    <ol>
+                        <li><p>拉 布</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/cutting"><img src="../imgs/1596784261.png"></a>
 
-                    <p>裁 剪</p>
+                    
+                    <ol>
+                        <li><p>裁 剪</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/ironing"><img src="../imgs/1597889061.png"></a>
 
-                    <p>整 燙 定 型</p>
+                    
+                    <ol>
+                        <li><p>整 燙 定 型</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/heatTransfer">
                         <img src="../imgs/1596784303.png">
                     </a>
-                    <p>轉 印</p>
+                    
+                    <ol>
+                        <li><p>轉 印</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/fusingPress">
                         <img src="../imgs/1598492647.png">
                     </a>
-                    <p>黏 合</p>
+                    
+                    <ol>
+                        <li><p>黏 合</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/seamless">
                         <img src="../imgs/1598492619.png">
                     </a>
 
-                    <p>無 縫 黏 合</p>
+                    
+                    <ol>
+                        <li><p>無 縫 黏 合</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/folding">
                         <img src="../imgs/1597908117.png">
                     </a>
-                    <p>摺 衣</p>
+                    
+                    <ol>
+                        <li><p>摺 衣</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/packaging">
                         <img src="../imgs/1596784354.png">
                     </a>
 
-                    <p>包 裝</p>
+                    
+                    <ol>
+                        <li><p>包 裝</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/needle">
                         <img src="../imgs/1621919665.png">
                     </a>
 
-                    <p>金 屬、重 量 檢 測</p>
+                    
+                    <ol>
+                        <li><p>金 屬、重 量 檢 測</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/needle">
                     <img src="../imgs/1608539845.png">
                     </a>
                     
-                    <p>鍋 爐</p>
+                    
+                    <ol>
+                        <li><p>鍋 爐</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/Needle">
                     <img src="../imgs/1596784420.png">
                     </a>
                     
-                    <p>其 它 機 械</p>
+                    
+                    <ol>
+                        <li><p>其 它 機 械</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
                 <li>
                     <a href="/Needle">
                     <img src="../imgs/主題-5.png">
                     </a>
                     
-                    <p>主 題 系 列</p>
+                    
+                    <ol>
+                        <li><p>主 題 系 列</p></li>
+                        <li><a href='https://.pngtree.com/so/圓形問號符號'><img src="../imgs/—Pngtree—round question mark symbol_4438523.png"></a></li>
+                    </ol>
                 </li>
             </ul>
         </div>
