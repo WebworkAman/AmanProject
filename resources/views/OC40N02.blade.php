5e1d26d67641e0951ca5e62cabbf2952fad21c96
webwork.aman <webwork.aman@gmail.com>
2022-11-25 10:38:11 +0800

    問題提交版面設計

--- resources/views/OC40N02.blade.php
+++ resources/views/OC40N02.blade.php
@@ -10,25 +10,32 @@
         <div class="QA_content">
         <div class="left-content">
             <img src="https://www.oshima.com.tw/archive/image/product1/images/layoutlist/1646990118.png">
+            <h2>人工智慧驗布機 OSHIMA EagleAi/Plus/Pro</h2>
+            <h3>縮短AI訓練 與眾不同</h3>
         </div>
         <div class="right-content">
               
-            @foreach($messages as $message)
+             <div class="message_border">
+             @foreach($messages as $message)
              <div class="message">
-            
+             
               <p>標題：{{ $message->title }}</p>
               <p>內容：{{ $message->content }}</p>
               <p>日期：{{ $message->created_at }}</p>
              </div>
              @endforeach
+             </div>
+             <div class="baseline"></div>
+             <h3>機器相關問題提交</h3>
               <form method="post" action="{{ route('post')}}" enctype="multipart/form-data">
                     @csrf
+                    
                     <p><label for="title">標題：</label></p>
                     <input type="title" name="title">
                     <p><label for="content">內文：</label></p>
                     <textarea id="content" name="content"  cols="30" rows="10"></textarea>
                     <p>
-                        <input type="submit" name="submit" value="Send">
+                        <input class='submit' type="submit" name="submit" value="Send">
                     </p>
               </form>
              
