676f2c6423af912345efb83f8c2de52640c7a23e
webwork.aman <webwork.aman@gmail.com>
2022-12-09 11:20:40 +0800

    信箱驗證-v1(寄出)

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Member;
 use App\Libraries\MemberAuth;
 
+
 class MemberController extends Controller
 {
     public function create()
@@ -28,6 +29,26 @@ class MemberController extends Controller
                 'email' => $request->email,
                 'password' => $request->password,
             ]);
+
+            $message = 'Dear <b>'.$request->name.'</b>';
+            $message.= '感謝您的註冊,我們需要驗證你的郵件信箱去完成帳號註冊';
+
+            $mail_data = [
+                'recipient'=>$request->email,
+                'fromEmail'=>$request->email,
+                'fromName'=>$request->name,
+                'subject'=>'會員註冊認證信',
+                'body'=> $message,
+                'actionLink'=>'www.google.com'
+            ];
+
+            \Mail::send('email-template',$mail_data,function($message)use($mail_data){
+                $message->to($mail_data['recipient'])
+                        ->from($mail_data['fromEmail'],$mail_data['fromName'])
+                        ->subject($mail_data['subject']);
+            });
+
+
 
             return back()->with('success','註冊成功，請到信箱收取你的驗證連結.');
         }else{
