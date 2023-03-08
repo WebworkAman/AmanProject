5847924c54fcf70ffe190c815f10edf20f261ba7
webwork.aman <webwork.aman@gmail.com>
2023-03-08 14:37:21 +0800

    調整底部連結

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,9 @@
 <footer>
         <div class="social_block">
             <ul>
-                <li><img src="../../imgs/Circle Button [SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE-1.png" alt=""></li>
-                <li><img src="../../imgs/Circle Button [SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE-2.png" alt=""></li>
-                <li><img src="../../imgs/Circle Button [SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE-3.png" alt=""></li>
-                <li><img src="../../imgs/Circle Button [SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE2].png"></li>
+                <li><a href="https://www.facebook.com/OshimaOfficial/"><img src="../../imgs/Circle Button [SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE-1.png"></a></li>
+                <li><a href="https://www.linkedin.com/company/oshima-official/mycompany/verification/"><img src="../../imgs/Circle Button [SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE-2.png"></a></li>
+                <li><a href="https://www.youtube.com/channel/UCBJ1IHvk8jBstBRfC5ldKKw"><img src="../../imgs/Circle Button [SIZE=MEDIUM][STATE=DEFAULT][STYLE=STYLE-3.png"></a></li>
             </ul>
 
             <p>Copyright © 2022 OSHIMA. All rights reserved.</p>
