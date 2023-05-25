8ec0d3f0b3f9ef040e08154c4cdd6fa4c50194d9
webwork.aman <webwork.aman@gmail.com>
2023-05-25 10:09:25 +0800

    忘記密碼畫面改版 0525

--- resources/views/members/forgotPW.blade.php
+++ resources/views/members/forgotPW.blade.php
@@ -7,22 +7,22 @@
     <link rel="stylesheet" href="{{asset('css/log.css')}}">
 </head>
 
-<body>
+<body class="UserLogin forgotPW" style="background-image: url('{{ asset('/imgs/photo/logbg.jpg') }}')">
 
     <main>
-    <h1 class="topText"><img src="{{asset('imgs/logo-01.png')}}"></h1>
+    <h1 class="topText"><img src="{{asset('imgs/photo/logo.png')}}"></h1>
         <div class="log forgot">
 
             <div class="left">
-            <h1>Forgot your password?</h1>
+            <h2>忘記密碼？</h2>
             <div class="baseline"></div>
-            <p>No problem.Just let us know your email address and we will email you a password reset link that will allow you to reset</p>
+            <p >沒問題！只需告訴我們您的電子郵件地址，我們將通過電子郵件向您發送一個密碼重置連結，您可以通過該連結重置密碼</p>
                                
                 <form method="post" action="{{route('forgot-password')}}">
                    @csrf
                 <div>
                       <label class="input">
-                          <p><input type="email" name="email" placeholder="Enter Email"
+                          <p><input type="email" name="email" placeholder="請輸入電子信箱"
                               value="{{old('email')}}"
                           ></p>
                           <br/>
@@ -31,9 +31,11 @@
                 </div>
                 
                 <div>
-                <button type="submit">
-                       Send Reset Password Link
-                </button>
+                <div class="button-style">
+                    <button type="submit">
+                       提 交
+                    </button>
+                </div>
                 </div>
                     @if(Session::has('success'))
                     <div class="alert alert-success">{{Session::get('success')}}</div>
--- resources/views/members/logIn.blade.php
+++ resources/views/members/logIn.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>log</title>
-    <link rel="stylesheet" href="../../css/log.css">
+    <link rel="stylesheet" href="{{asset('css/log.css')}}">
 </head>
 
 <body class="UserLogin" style="background-image: url('{{ asset('/imgs/photo/logbg.jpg') }}')">
