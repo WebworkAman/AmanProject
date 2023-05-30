174fd4103dcc5402f78c8b2972ae026541f49436
webwork.aman <webwork.aman@gmail.com>
2023-05-30 10:14:06 +0800

    授權全部權限功能更新/控制管理畫面優化  0530

--- resources/views/admin/Member/member-permissions.blade.php
+++ resources/views/admin/Member/member-permissions.blade.php
@@ -12,15 +12,23 @@
          <div class="Show_ reply">
 
          <div class="Show_form member-permissions">
-                    <h3> 權 限 管 理 </h3>
-                    <a class="btn control-option" href="{{route('members.adminCreate')}}">提交更新</a>
+                    <div class="text-block">
+                        <h3> 權 限 管 理 </h3>
+                        <a class="btn" href="{{ asset('admin/index') }}">返回會員</a>
+                    </div>
+
                     <form action="{{ route('member.permissions.update',$member->id) }}" method="POST">
                          @csrf
 
-                         <!-- 显示所有产品的复选框 -->
+                         <!-- 顯示所有產品的複選框 -->
  
-                     <div class="checkSelect">
-                     <ul>
+                    <div class="checkSelect">
+                       <label class="selectAll">
+                                <input type="checkbox" id="select-all">
+                                授權所有產品權限
+                       </label>
+                       <div class="baseline"></div>
+                     <ul id="product-list">
                      @foreach ($products as $product)
                                  <li>
                                      <label>
@@ -34,9 +42,9 @@
                                  </li>
                      @endforeach
                      </ul>
-                 </div>
+                     </div>
 
-
+                      
                       <button type="submit">更新權限</button>
                   </form>
  
@@ -51,6 +59,19 @@
    @php
     // 將 $memberPermissions 的值輸出到控制台
     echo "<script>console.log('memberPermissions:', " . json_encode($memberPermissions) . ");</script>";
-@endphp
+   @endphp
+
+   <script>
+    $(document).ready(function(){
+        // 監聽 "All" 選項的變更事件
+           $('#select-all').change(function(){
+               var isChecked = $(this).prop('checked');
+               
+               //設定所有產品選項的勾選狀態與 "All" 選項一致
+               $('#product-list input[type="checkbox"]').prop('checked',isChecked);
+
+           });
+    })
+   </script>
     
    @endsection
