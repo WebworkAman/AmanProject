e5a22bc479e65d7f0ac4e1700b66188285d8164d
webwork.aman <webwork.aman@gmail.com>
2023-09-13 16:23:19 +0800

    使用者列表添加權限說明/按鈕高度修正

--- resources/views/admin/Admin/admin-list.blade.php
+++ resources/views/admin/Admin/admin-list.blade.php
@@ -6,6 +6,14 @@
             <h3> 使 用 者 列 表 </h3>
             <a class="btn control-option" href="{{ route('admin.create') }}">新增</a>
             <a class="btn control-option" href="{{ route('admin.Status') }}">修改</a>
+            <div class="textblock">
+                <p>售後為最高單位權限，可使用全功能</p>
+
+                <p>技術單位權限，無法使用收信設定</p>
+                <p>客服單位權限，無法使用收信和管理者設定</p>
+                <p>請留意，若更改單位為無，則無法登入後台</p>
+            </div>
+
             <table>
                 <thead>
                     <tr>
@@ -13,7 +21,7 @@
                         <th>姓名</th>
                         <th>信箱</th>
 
-                        <th></th>
+                        {{-- <th></th> --}}
                     </tr>
                 </thead>
                 <tbody>
@@ -35,17 +43,18 @@
                             </td>
                             <td>{{ $admin->email }}</td>
 
-                            <td>
+                            {{-- <td>
                                 <form method="POST" action="{{ route('admin.destroy', $admin->id) }}">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="delete-button">刪除</button>
                                 </form>
-                            </td>
+                            </td> --}}
                         </tr>
                     @endforeach
                 </tbody>
             </table>
+
             @stack('scripts')
         </div>
     </div>
